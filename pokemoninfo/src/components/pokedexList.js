2dc73abfc19243a12719d4d6187bd2d057df42b3
Raymond Huang <rhuang6@mail.sfsu.edu>
2024-02-07 15:06:54 -0800

    No Changes

--- pokemoninfo/src/components/pokedexList.js
+++ pokemoninfo/src/components/pokedexList.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { fetchAllPokemon } from "./api"
-import { fetchPokemonImages } from "./api"
+// import { fetchPokemonImages } from "./api"
 
 const PokedexList = () => {
 
@@ -33,25 +33,23 @@ const PokedexList = () => {
     //     getAllImage();
     // }, [])
 
-
-    console.log(fetchAllPokemon);
     return (
         <div>
             {/* Navbar */}
-            <nav class="pt-4 bg-black">
-                <a class="navbar-brand" href="">
+            <nav className="pt-4 bg-black">
+                <a className="navbar-brand" href="">
                     <img src="PokemonLogo.png" width="150" height="160" alt="Pokemon Logo" />
                 </a>
             </nav>
 
             {/* Search Bar */}
-            <nav class="navbar navbar-light bg-light">
-                <form class="form-inline">
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1"><img src="Pokeball.png" width="30" height="50" alt="Pokeball"></img></span>
+            <nav className="navbar navbar-light bg-light">
+                <form className="form-inline">
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <span className="input-group-text" id="basic-addon1"><img src="Pokeball.png" width="30" height="50" alt="Pokeball"></img></span>
                         </div>
-                        <input type="text" class="form-control" placeholder="Search Pokemon" aria-label="Username" aria-describedby="basic-addon1" />
+                        <input type="text" className="form-control" placeholder="Search Pokemon" aria-label="Username" aria-describedby="basic-addon1" />
                     </div>
                 </form>
             </nav>
@@ -62,7 +60,7 @@ const PokedexList = () => {
                     {allPokemon.map((pokemon, _id) => (
                         <div key={_id}>
                             <div className="ecommerce_border_color rounded-xl lg-shadow w-full h-full flex flex-col justify-between p-12">
-                                <img className="w-28 h-28 lg:w-32 lg:h-32 mx-auto" src={pokemon.sprites} alt="product"></img>
+                                <img className="w-28 h-28 lg:w-32 lg:h-32 mx-auto" alt="product"></img>
                                 <h1 className="text-base md:text-xl mt-10">Bed Rooms: 2 | 2 Floors | 2810 Sqft House on Sale </h1>
                                 <p className="mobile-responsive-fontprice-product robotoFont font-bold text-base md:text-lg pt-4 price-color">{pokemon.name}</p>
                             </div>
