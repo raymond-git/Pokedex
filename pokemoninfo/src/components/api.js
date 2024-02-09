c50d465719066559baa1645fa5485595dfc17489
Raymond Huang <raymondhuang615@yahoo.com>
2024-02-08 21:29:12 -0800

    Added pokemon images

--- pokemoninfo/src/components/api.js
+++ pokemoninfo/src/components/api.js
@@ -9,12 +9,3 @@ export const fetchAllPokemon = async () => {
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=150', options)
     return response.json();
 }
-
-// export const fetchPokemonId = async (id) => {
-//     const options = {
-//         method: 'GET',
-//         header: { 'Content-Type': 'application/json'}
-//     }
-//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, options)
-//     return response.json();
-// }
--- pokemoninfo/src/components/pokedexList.js
+++ pokemoninfo/src/components/pokedexList.js
@@ -1,39 +1,33 @@
 import { useEffect, useState } from "react";
 import { fetchAllPokemon } from "./api"
-import { fetchPokemonImages } from "./api"
 
 const PokedexList = () => {
 
     const [allPokemon, setAllPokemon] = useState([]);
+    const [handleInput, setHandleInput] = useState([]);
 
     useEffect(() => {
         async function getAllPokemon() {
             try {
                 const response = await fetchAllPokemon();
-                setAllPokemon(response.results);
-                // console.log(response.results)
+                const pokemons = response.results;
+                const PokemonUrls = pokemons.map(pokemon => pokemon.url);
+                const response2 = await Promise.all(PokemonUrls.map(url => fetch(url)));
+                const data = await Promise.all(response2.map(response => response.json()));
+                setAllPokemon(data)
+                console.log(data);
             } catch (error) {
                 console.error('Error Fetching Pokemon Data')
             }
         }
         getAllPokemon();
-    }, [])
+    }, []);
 
-    // useEffect(() => {
-    //     async function getAllImages() {
-    //         try {
-    //             const response = await fetchPokemonImages();
-    //             setAllPokemonImages(response.results);
-    //             console.log(response.results);
-    //         } catch (error) {
-    //             console.error('Error Fetching Pokemon Data')
-    //         }
-    //     }
-    //     getAllImage();
-    // }, [])
-
-
-    console.log(fetchAllPokemon);
+    async function handleChange(event) {
+        setHandleInput(event.target.value);
+        console.log(event.target.value);
+    }
+     
     return (
         <div>
             {/* Navbar */}
@@ -48,20 +42,20 @@ const PokedexList = () => {
                 <form className="form-inline">
                     <div className="input-group">
                         <div className="input-group-prepend">
-                            <span className="input-group-text" id="basic-addon1"><img src="Pokeball.png" width="30" height="50" alt="Pokeball"></img></span>
+                            <span className="input-group-text" id="pokeball"><img src="Pokeball.png" width="30" height="50" alt="Pokeball"></img></span>
                         </div>
-                        <input type="text" className="form-control" placeholder="Search Pokemon" aria-label="Username" aria-describedby="basic-addon1" />
+                        <input type="text" value={handleInput} onChange={handleChange} className="form-control" id="pokemonresults" placeholder="Search Pokemon" aria-label="Username" aria-describedby="basic-addon1" />
                     </div>
                 </form>
             </nav>
 
             <div className='flex justify-evenly'>
                 <div className="grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-
                     {allPokemon.map((pokemon, _id) => (
                         <div key={_id}>
                             <div className="ecommerce_border_color rounded-xl lg-shadow w-full h-full flex flex-col justify-between p-12">
-                                <img className="w-28 h-28 lg:w-32 lg:h-32 mx-auto" src={pokemon.sprites} alt="product"></img>
+                                <p>{pokemon.id}</p>
+                                <img className="w-28 h-28 lg:w-72 lg:h-52 mx-auto" src={pokemon.sprites.other.dream_world.front_default} alt="product"></img>
                                 <h1 className="text-base md:text-xl mt-10">Bed Rooms: 2 | 2 Floors | 2810 Sqft House on Sale </h1>
                                 <p className="mobile-responsive-fontprice-product robotoFont font-bold text-base md:text-lg pt-4 price-color">{pokemon.name}</p>
                             </div>
