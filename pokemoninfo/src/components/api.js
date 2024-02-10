233ca97d967bbf2d40db6c55fd2ba70877427395
Raymond Huang <raymondhuang615@yahoo.com>
2024-02-09 17:37:31 -0800

    Work on searchbar data

--- pokemoninfo/src/components/api.js
+++ pokemoninfo/src/components/api.js
@@ -1,11 +1,19 @@
 import fetch from 'node-fetch';
 
 export const fetchAllPokemon = async () => {
-
     const options = {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' }
     }
     const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=20&limit=150', options)
+    return response.json();
+}
+
+export const fetchSinglePokemon = async (pokemonName) => {
+    const options = {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    }
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`, options)
     return response.json();
 }
--- pokemoninfo/src/components/pokedexList.js
+++ pokemoninfo/src/components/pokedexList.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import { fetchAllPokemon } from "./api"
+import { fetchSinglePokemon } from "./api";
 
 const PokedexList = () => {
 
     const [allPokemon, setAllPokemon] = useState([]);
-    const [handleInput, setHandleInput] = useState([]);
+    const [singlePokemon, setSinglePokemon] = useState([]);
+    const [userInput, setUserInput] = useState([]);
+    const [handleTrue, setHandleTrue] = useState("false"); //handleTrue = false
 
     useEffect(() => {
         async function getAllPokemon() {
@@ -14,8 +17,8 @@ const PokedexList = () => {
                 const PokemonUrls = pokemons.map(pokemon => pokemon.url);
                 const response2 = await Promise.all(PokemonUrls.map(url => fetch(url)));
                 const data = await Promise.all(response2.map(response => response.json()));
-                setAllPokemon(data)
-                console.log(data);
+                setAllPokemon(data);
+                setHandleTrue(false);
             } catch (error) {
                 console.error('Error Fetching Pokemon Data')
             }
@@ -23,46 +26,77 @@ const PokedexList = () => {
         getAllPokemon();
     }, []);
 
-    async function handleChange(event) {
-        setHandleInput(event.target.value);
-        console.log(event.target.value);
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const pokemonName = userInput;
+            const pokemonData = await fetchSinglePokemon(pokemonName);
+            console.log(pokemonData);
+            setSinglePokemon(pokemonData);
+            setHandleTrue(true);
+        } catch (error) {
+            console.log("Error handling Pokémon data:", error);
+        }
     }
-     
+
+    async function handleChange(event) {
+        setUserInput(event.target.value);
+    }
+
+    const handleClearUser = () => {
+        setSinglePokemon(allPokemon); // Reset singlePokemon state to the original list of all Pokémon
+    };
+
     return (
         <div>
             {/* Navbar */}
             <nav className="pt-4 bg-black">
-                <a className="navbar-brand" href="">
+                <a className="navbar-brand">
                     <img src="PokemonLogo.png" width="150" height="160" alt="Pokemon Logo" />
                 </a>
             </nav>
 
             {/* Search Bar */}
             <nav className="navbar navbar-light bg-light">
-                <form className="form-inline">
+                <form onSubmit={handleSubmit} className="form-inline">
                     <div className="input-group">
                         <div className="input-group-prepend">
                             <span className="input-group-text" id="pokeball"><img src="Pokeball.png" width="30" height="50" alt="Pokeball"></img></span>
                         </div>
-                        <input type="text" value={handleInput} onChange={handleChange} className="form-control" id="pokemonresults" placeholder="Search Pokemon" aria-label="Username" aria-describedby="basic-addon1" />
+                        <input type="text" onKeyUp={handleClearUser} value={userInput} onChange={handleChange} className="form-control" id="pokemonresults" placeholder="Search Pokemon" aria-label="Username" aria-describedby="basic-addon1" />
                     </div>
                 </form>
             </nav>
 
-            <div className='flex justify-evenly'>
-                <div className="grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                    {allPokemon.map((pokemon, _id) => (
-                        <div key={_id}>
+            {!handleTrue ? ( // Render allPokemon if handleTrue is false
+                <div className='flex justify-evenly'>
+                    <div className="grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                        {allPokemon.map((pokemon, _id) => (
+                            <div key={_id}>
+                                <div className="ecommerce_border_color rounded-xl lg-shadow w-full h-full flex flex-col justify-between p-12">
+                                    <p>{pokemon.id}</p>
+                                    <img className="w-28 h-28 lg:w-72 lg:h-52 mx-auto" src={pokemon.sprites.other.dream_world.front_default} alt="pokemon" />
+                                    <h1 className="text-base md:text-xl mt-10">Bed Rooms: 2 | 2 Floors | 2810 Sqft House on Sale </h1>
+                                    <p className="mobile-responsive-fontprice-product robotoFont font-bold text-base md:text-lg pt-4 price-color">{pokemon.name}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ) : ( // Render single Pokémon if handleTrue is true
+                <div className='flex justify-evenly'>
+                    <div className="grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                        <div>
                             <div className="ecommerce_border_color rounded-xl lg-shadow w-full h-full flex flex-col justify-between p-12">
-                                <p>{pokemon.id}</p>
-                                <img className="w-28 h-28 lg:w-72 lg:h-52 mx-auto" src={pokemon.sprites.other.dream_world.front_default} alt="product"></img>
+                                <p>{singlePokemon.id}</p>
+                                <img className="w-28 h-28 lg:w-72 lg:h-52 mx-auto" src={singlePokemon?.sprites?.other.dream_world.front_default} alt="individual-pokemon" />
                                 <h1 className="text-base md:text-xl mt-10">Bed Rooms: 2 | 2 Floors | 2810 Sqft House on Sale </h1>
-                                <p className="mobile-responsive-fontprice-product robotoFont font-bold text-base md:text-lg pt-4 price-color">{pokemon.name}</p>
+                                <p className="mobile-responsive-fontprice-product robotoFont font-bold text-base md:text-lg pt-4 price-color">{singlePokemon.name}</p>
                             </div>
                         </div>
-                    ))}
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
